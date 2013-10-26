49114c0400a2374cb0996f61f35229c7055c8eca
Tatiana Charkova <tattash@gmail.com>
2013-10-25 21:21:57 -0700

    Added a view.

---
+++ app/scripts/views/job.js
@@ -0,0 +1,14 @@
+/*global developHer, Backbone, JST*/
+
+developHer.Views = developHer.Views || {};
+
+(function () {
+    'use strict';
+
+    developHer.Views.JobView = Backbone.View.extend({
+
+        template: JST['app/scripts/templates/job.ejs']
+
+    });
+
+})();

ed821a1bcd7869069efaad75bd1a4e798d0f429d
Tatiana Charkova <tattash@gmail.com>
2013-10-26 10:27:34 -0700

    Moved fake data into collection.

--- app/scripts/collections/jobs.js
+++ app/scripts/collections/jobs.js
@@ -13,4 +13,90 @@ developHer.Collections = developHer.Collections || {};
 
     window.jobsCollection = new developHer.Collections.JobsCollection();
 
+    var job1 = new developHer.Models.JobModel({
+		type: 'agriculture',
+		period: 'May - September',
+		salary: "$1000",
+		description: "farmWork",
+		position: {
+          latitude: 37.591927,
+          longitude: -122.34375
+        },
+        contact: '(415)-357-38-98'
+	});
+
+    var job2 = new developHer.Models.JobModel({
+		type: 'construction',
+		period: '3 months',
+		salary: "$1000",
+		description: "construction work",
+		position: {
+          latitude: 38,
+          longitude: -122
+        },
+        contact: '(415)-357-38-98'
+	});
+
+	var job3 = new developHer.Models.JobModel({
+		type: 'custodian',
+		period: '2 weeks',
+		salary: "$1070",
+		description: "custodian work",
+		position: {
+          latitude: 39,
+          longitude: -122
+        },
+        contact: '(415)-357-38-98'
+	});
+
+	var job4 = new developHer.Models.JobModel({
+		type: 'government',
+		period: '-',
+		salary: "$100",
+		description: "government work",
+		position: {
+          latitude: 37,
+          longitude: -121
+        },
+        contact: '(415)-357-38-98'
+	});
+
+	var job5 = new developHer.Models.JobModel({
+		type: 'childcare',
+		period: 'permanent',
+		salary: "$1200",
+		description: "childcare work",
+		position: {
+          latitude: 37,
+          longitude: -122.89
+        },
+        contact: '(415)-357-38-98'
+	});
+
+	var job6 = new developHer.Models.JobModel({
+		type: 'factory',
+		period: '1 year',
+		salary: "$11/hour",
+		description: "factory work",
+		position: {
+          latitude: 37.5919,
+          longitude: -121.8375
+        },
+        contact: '(415)-357-38-98'
+	});
+
+	var job7 = new developHer.Models.JobModel({
+		type: 'manuallabor',
+		period: '20 hours',
+		salary: "$300",
+		description: "lifting boxes",
+		position: {
+          latitude: 36,
+          longitude: -122.8
+        },
+        contact: '(415)-357-38-98'
+	});
+
+	jobsCollection.add([job1, job2, job3, job4, job5, job6, job7]);
+
 })();
--- app/scripts/views/jobs.js
+++ app/scripts/views/jobs.js
@@ -1,6 +1,6 @@
 /*global developHer, Backbone, JST*/
 
-var loadMap = function(){
+loadMap = function(){
   var map;
 
   function initialize() {
@@ -11,104 +11,20 @@ var loadMap = function(){
       zoomControl: true
     }
 
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
-
-    var icons = {
-      agriculture: {
-        icon: './markers/agriculture.png',
-        sound: 'agriculture',
-        position: {
-          latitude: 37.591927,
-          longitude: -122.34375
-        },
-        salary: "$1000",
-        description: "farmWork",
-        phoneNumber: "(555)239-2903"
-      },
-      factory: {
-        icon: './markers/factory.png',
-        sound: 'factory',
-        position: {
-          latitude: 37.5919,
-          longitude: -121.8375
-        },
-        salary: "$250",
-        description: "factoryWork",
-        phoneNumber: "(888)919-1223"
-      },
-      construction: {
-        icon: './markers/construction.png',
-        sound: 'construction',
-        position: {
-          latitude: 38,
-          longitude: -122
-        },
-        salary: "$100",
-        description: "constructionWork",
-        phoneNumber: "(998)219-3427"
-      },
-      custodian: {
-        icon: './markers/custodian.png',
-        sound: 'custodian',
-        position: {
-          latitude: 39,
-          longitude: -122
-        },
-        salary: "$10",
-        description: "custodianWork",
-        phoneNumber: "(192)938-2113"
-      },
-      government: {
-        icon: './markers/government.png',
-        sound: 'government',
-        position: {
-          latitude: 37,
-          longitude: -121
-        },
-        salary: "$500",
-        description: "govtWork",
-        phoneNumber: "(290)109-1203"
-      },
-      childcare: {
-        icon: './markers/childcare.png',
-        sound: 'childcare',
-        position: {
-          latitude: 37,
-          longitude: -122.89
-        },
-        salary: "$2500",
-        description: "childcareWork",
-        phoneNumber: "(123)312-9323"
-      },
-      manuallabor: {
-        icon: './markers/manuallabor.png',
-        sound: 'manuallabor',
-        position: {
-          latitude: 36,
-          longitude: -122.8
-        },
-        salary: "$200",
-        description: "manuallaborWork",
-        phoneNumber: "(121)239-2982"
-      }
-    };
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
     var addMarker = function(feature) {
-      var pos = new google.maps.LatLng(feature.position.latitude,
-                                         feature.position.longitude);
+      var pos = new google.maps.LatLng(feature.get('position').latitude, feature.get('position').longitude);
       var marker = new google.maps.Marker({
         position: pos,
-        // icon: icons[feature.type].icon,
-        icon: feature.icon,
+        icon: ('./markers/' + feature.get('type') + '.png' ),
         map: map
       });
       google.maps.event.addListener(marker, 'mouseover', function() {
-        playSound(feature.sound);
+        playSound(feature.get('type'));
       });
 
       google.maps.event.addListener(marker, 'click', function() {
-        console.log(feature.sound)
         showModal(feature)
       });
 
@@ -116,11 +32,10 @@ var loadMap = function(){
     // Try HTML5 geolocation
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = new google.maps.LatLng(position.coords.latitude,
-                                         position.coords.longitude);
-        for (var icon in icons) {
-          addMarker(icons[icon]);
-        }
+        var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        _.each(jobsCollection.models, function(jobModel) {
+          addMarker(jobModel);
+        });
         
         var myLocation = new google.maps.Marker({
             position: pos,
@@ -162,10 +77,10 @@ var loadMap = function(){
   window.showModal = function(feature){
     $('#newModal').modal('toggle');
     
-    $('.md-title').html("<img src='"+feature.icon+"'>")
+    $('.md-title').html("<img src='./markers/" + feature.get('type') + ".png'>")
     $('.md-title').unbind( "mouseover" );
     $('.md-title').on('mouseover', function(){
-      playSound(feature.sound)
+      playSound(feature.get('type'))
     });
     
     $('.md-text').text(feature.salary)
@@ -173,10 +88,10 @@ var loadMap = function(){
     $('.md-description').html('<img src="./markers/wat.png">')
     $('.md-description').unbind( "mouseover" );
     $('.md-description').on('mouseover', function(){
-      playSound(feature.description)
+      playSound(feature.get('description'))
     });
 
-    $('.md-phone').html('<img src="./markers/phone.png"><span>'+feature.phoneNumber+'</span>')
+    $('.md-phone').html('<img src="./markers/phone.png"><span>'+feature.get('contact')+'</span>')
 
   };
   
